c515431f0bc4d1ef7e0f7a34c2f052e568623bcb
Metaimperiya <ddos.atack.service@gmail.com>
2026-01-10 04:34:54 +0300

    Gotovo

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('mainBtn');
+    btn.addEventListener('click', () => {
+        alert('СИСТЕМА METAMPERIYA АКТИВИРОВАНА!');
+    });
+});
